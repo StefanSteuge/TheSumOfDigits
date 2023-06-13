e3e82d9daf23637c6acb460ef4fd5c9a88ae326d
StefanSteuge <fotosteuge@gmail.com>
2023-06-13 20:44:22 +0300

    add code for ForLoop class

--- src/ByForLoop.java
+++ src/ByForLoop.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class ByForLoop {
+
+    public static void main(String[] args) {
+        long number, sum;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter a number: ");
+        number = scanner.nextLong();
+        for (sum = 0; number != 0; number /= 10) {
+            sum += number % 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
 }
